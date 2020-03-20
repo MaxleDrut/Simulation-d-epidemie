cde819246d409ddc6471a7a1cc6bcb75f2cf7307
Maxence DRUTEL <49818023+Shikobaa@users.noreply.github.com>
2020-03-20 13:01:58 +0100

    Implémentation du timer dans la simulation
    Il reste encore quelques ajouts à faire mais le système de timer est fonctionnel

---
+++ EcouteurVitesse.java
@@ -0,0 +1,29 @@
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class EcouteurVitesse implements ActionListener{
+    private Simulation simu;
+    private String type;
+
+    public EcouteurVitesse(Simulation s, String t){
+        simu = s;
+        type = t;
+    }
+
+    public void actionPerformed(ActionEvent e){
+       switch(type) {
+		   case ("pause"):
+		   simu.pauseTimer();
+		   break;
+		   case ("acc"):
+		   simu.changerVit(-1);
+		   break;
+		   case ("ral"):
+		   simu.changerVit(1);
+		   break;
+		   default:
+	   }
+       
+    }
+}
--- Simulation.java
+++ Simulation.java
@@ -1,54 +1,110 @@
-import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class Simulation {
+public class Simulation extends JFrame implements ActionListener {
 	
-	private static Virus maladie;
-	//private static Region zone;
-	protected static int tempsSimu;
+	private Virus maladie;
+	private Region zone;
+	protected int tempsSimu;
 	
-	private static long delay = 1000; 
-	private static Timer temps = new Timer();
-	private static TimerJeu jeu = new TimerJeu();
+	private boolean timerOn;
+	private int[] delais = {50,100,200,500,1000,2000,5000};
+	private int delaiActuel;
+	private int delaiRef;
+	private Timer temps;
 	
+	private JPanel pHaut,pBas;
+	private JButton bPause, bAcc, bRal;
+	private TextField afficheurVit;
 	
-	//TODO : main va se charger de créer le virus, de créer la région, d'infecter les premières zones et d'instancier le JFrame de la simulation
-	public static void main (String[] args) {
-		tempsSimu = 0;
-		changerVit(1000);
+	public Simulation(int x, int y) {
+		super("Simulation d'epidemie");
+		
+		delaiActuel = delais.length-3;
+		temps = new Timer(1, this); //1 correspond ici au délai par défaut pour les changement de vitesse, et non le délai initial
+		delaiRef = delais[delaiActuel];
+		temps.setDelay(delaiRef);
+		timerOn = false;
+		
+		//Panel du haut
+		pHaut = new JPanel();
+		afficheurVit = new TextField(50);
+		afficheurVit.setEditable(false);
+		afficherVitesse();
+		pHaut.add(afficheurVit);
+		
+		
+		//Panel du bas
+		pBas = new JPanel();
+		bPause = new JButton("Start");
+		bAcc = new JButton("Accelerer");
+		bRal = new JButton("Ralentir");
+		
+		bPause.addActionListener(new EcouteurVitesse(this, "pause"));
+		bAcc.addActionListener(new EcouteurVitesse(this,"acc"));
+		bRal.addActionListener(new EcouteurVitesse(this,"ral"));
+		
+		pBas.add(bRal);
+		pBas.add(bPause);
+		pBas.add(bAcc);
+		
+		//Ajout des panels
+		add(pHaut, BorderLayout.NORTH);
+		add(pBas, BorderLayout.SOUTH);
+		
+		this.setSize(x,y);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		
 	}
 	
-	
-	public static void pauseTimer() {
-		
-		
-	}
-	
-	public static void changerVit(int v) { 
-		delay = v;
-		temps.cancel();
-		temps.purge();
-		
-		temps = new Timer();
-		jeu = new TimerJeu();
-		temps.scheduleAtFixedRate(jeu, 0, delay);
-		
-		
-	}
-	
-	
-	/* Une fois un certan temps écoulé avec les timers,
-	 * la simu va incrémenter de 1 la variable de temps, et a exécuter les évènements
-	 * liés à l'avancement dans le temps (infection, deplacements, mutation)
-	 */
-	 
-	public static void jourSuivant() {
-		
-		
+	public void actionPerformed(ActionEvent ae) { //Chaque incrémentation fait passer au jour suivant.
 		tempsSimu++;
 		System.out.println(tempsSimu);
+	}
+	
+	public void pauseTimer() {
+		if(timerOn) {
+			temps.stop();
+			bPause.setText("Start");
+		} else {
+			temps.start();
+			bPause.setText("Stop");
+		}
+		timerOn = !timerOn;
+		this.validate();
+		this.repaint();
+	}
+	
+	public void changerVit(int commande) { //Accélérer envoie -1, ralentir envoie +1
+		if(!(commande==1 && delaiActuel==delais.length-1) && !(commande==-1 && delaiActuel==0)) { //Pour éviter les outOfBounds
+			delaiActuel+=commande;
+			temps.setDelay(delais[delaiActuel]);
+			if(timerOn) {
+				temps.start();
+			}
+			afficherVitesse();
+		}
+	}
+	
+	/* Rapport des vitesse = 1/rapport des delais = delai de reference/delai actuel
+	 * Donc vitesse x2 veut dire délai 2 fois plus faible que celui de référence
+	 */
+	
+	public void afficherVitesse() {
+		double rapportVitesse = ((double) (delaiRef))/delais[delaiActuel];
+		afficheurVit.setText("Vitesse actuelle : x"+rapportVitesse);
+		this.validate();
+		this.repaint();
+	}
+	
+	public static void main (String[] args) {
 		
-	} 
+		Simulation simu = new Simulation(500,500);
+		
+	}
 	
 }
 
