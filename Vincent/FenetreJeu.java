4d344febc0e2c890431e419186915b92bedc2624
CodeXXX <vince.leboz@gmail.com>
2020-03-29 18:55:31 +0400

    Update FenetreJeu.java

--- Vincent/FenetreJeu.java
+++ Vincent/FenetreJeu.java
@@ -98,7 +98,8 @@ public class FenetreJeu extends JFrame implements MouseListener{
           
         //Effets des boutons
         
-        //boutonLancement.addActionListener( new EcouteurClic(this, 1));
+        //
+        boutonLancement.addActionListener( new EcouteurClic(this, 1));
 		boutonLancement.addMouseListener(this);
 		titre.addMouseListener(this);
 		titre.setHighlighter(null);
