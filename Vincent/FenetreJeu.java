04da805e957817c65882cffdfd17510e05686048
CodeXXX <vince.leboz@gmail.com>
2020-03-29 19:49:43 +0400

    ajout canada

--- Vincent/FenetreJeu.java
+++ Vincent/FenetreJeu.java
@@ -99,7 +99,7 @@ public class FenetreJeu extends JFrame implements MouseListener{
         //Effets des boutons
         
         //
-        boutonLancement.addActionListener( new EcouteurClic(this, 1));
+        //boutonLancement.addActionListener( new EcouteurClic(this, 1));
 		boutonLancement.addMouseListener(this);
 		titre.addMouseListener(this);
 		titre.setHighlighter(null);
