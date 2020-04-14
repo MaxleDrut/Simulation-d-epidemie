b4dd062b7770921bffc4c78d60c19cb822c4ba0e
Maxence DRUTEL <49818023+Shikobaa@users.noreply.github.com>
2020-04-14 16:02:39 +0200

    Modif barre statistiques

--- BarreStatistiques.java
+++ BarreStatistiques.java
@@ -7,17 +7,41 @@ public class BarreStatistiques extends JPanel {
 	double[] proportions;
 	
 	public void paintComponent(Graphics g){
-		if(proportions==null) {
 			double[] proportions = {1.0,0.0,0.0,0.0};
-		}
+		int i=0;
+		System.out.println("cc");
+		int seuilInf = 0;
+		int seuilSup = (int) (this.getWidth()*proportions[i]);
 		g.setColor(Color.GREEN);
-		g.fillRect(0,0,(int) (this.getWidth()*proportions[0]),this.getHeight());
+		g.fillRect(seuilInf,0,seuilSup,this.getHeight());
+		
+		i++;
+		seuilInf = seuilSup;
+		seuilSup+=(int) (this.getWidth()*proportions[i]);
+		g.setColor(Color.RED);
+		g.fillRect(seuilInf,0,seuilSup,this.getHeight());
+		
+		i++;
+		seuilInf = seuilSup;
+		seuilSup+=(int) (this.getWidth()*proportions[i]);
 		g.setColor(Color.BLUE);
-		g.fillRect(this.getWidth()/2,0,this.getWidth(),this.getHeight());
+		g.fillRect(seuilInf,0,seuilSup,this.getHeight());
+		
+		i++;
+		seuilInf = seuilSup;
+		seuilSup+=(int) (this.getWidth()*proportions[i]);
+		g.setColor(Color.DARK_GRAY);
+		g.fillRect(seuilInf,0,seuilSup,this.getHeight());
 	}
 	
-	public void setProportions(double[] vals) {
-		proportions = vals;
+	public void setProportions(int[] vals) {
+		int total=0;
+		for(int i=0;i<proportions.length;i++) {
+			total+=vals[i];
+		}
+		for(int i=0;i<proportions.length;i++) {
+			proportions[i]=(double)(vals[i])/total;
+		}
 	}
                  
 }
